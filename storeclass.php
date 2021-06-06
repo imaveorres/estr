29d4d481374f617bf023fd8612b0cd3ead75406d
kim <torres.kim.dave@gmail.com>
2021-06-06 14:45:47 +0800

    get users strategy

--- storeclass.php
+++ storeclass.php
@@ -8,6 +8,8 @@ class MyStore
  private $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC);
  protected $con;
 
+
+ // open conn
  public function openConnection()
  {
   try {
@@ -19,8 +21,27 @@ class MyStore
   }
  }
 
+
+ // close conn
  public function closeConnection()
  {
   $this->con = null;
+ }
+
+
+ // get users 
+ public function getUsers()
+ {
+  $connection = $this->openConnection();
+  $stmt = $connection->prepare("SELECT * FROM members");
+  $stmt->execute();
+  $users = $stmt->fetchAll();
+  $userCount = $stmt->rowCount();
+
+  if ($userCount > 0) {
+   return $users;
+  } else {
+   return 0;
+  }
  }
 }
