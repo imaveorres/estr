dbf502ce21bfadd96dfbb3456775de80f5de1897
kim <torres.kim.dave@gmail.com>
2021-06-06 14:07:30 +0800

    PDO open connection function

---
+++ storeclass.php
@@ -0,0 +1,21 @@
+<?php
+
+class MyStore
+{
+ private $server = "mysql:host=localhost;dbname=mystore";
+ private $user =  "root";
+ private $pass = "";
+ private $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC);
+ protected $con;
+
+ public function openConnection()
+ {
+  try {
+   $this->conn = new PDO($this->server, $this->user, $this->pass, $this->options);
+   return $this->conn;
+  } catch (PDOException $e) {
+
+   echo "There is some problem in the connection : " . $e->getMessage();
+  }
+ }
+}
