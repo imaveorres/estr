6d02a59fed12e8546fc92dbfbc33d0c9fd39c1d5
kim <torres.kim.dave@gmail.com>
2021-06-11 22:22:23 +0800

    changes UI

--- login.php
+++ login.php
@@ -11,32 +11,39 @@ $store->login();
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <title>Login</title>
  <link rel="stylesheet" href="node_modules/bulma/css/bulma.min.css">
+ <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
 </head>
 
 <body>
  <div class="container">
   <div class="login-form">
-   <div class="field">
-    <label class="label">Username</label>
-    <div class="control has-icons-left">
-     <input class="input" type="text" name="username" id="username">
-     <span class="icon is-small is-left"><i class="fas fa-user"></i></span>
+   <form action="" method="post">
+    <div class="field">
+     <label class="label">Username</label>
+     <div class="control has-icons-left">
+      <input class="input" type="text" name="username" id="username">
+      <span class="icon is-small is-left">
+       <i class="fas fa-user"></i>
+      </span>
+     </div>
     </div>
-   </div>
 
-   <div class="field">
-    <label class="label">Password</label>
-    <div class="control has-icons-left">
-     <input class="input" type="password" name="password" id="password">
-     <span class="icon is-small is-left"><i class="fas fa-lock"></i></span>
+    <div class="field">
+     <label class="label">Password</label>
+     <div class="control has-icons-left">
+      <input class="input" type="password" name="password" id="password">
+      <span class="icon is-small is-left">
+       <i class="fas fa-lock"></i>
+      </span>
+     </div>
     </div>
-   </div>
 
-   <div class="field">
-    <div class="control">
-     <button class="button is-link" type="submit" name="submit">Login</button>
+    <div class="field">
+     <div class="control">
+      <button class="button is-link" type="submit" name="submit">Login</button>
+     </div>
     </div>
-   </div>
+   </form>
   </div>
  </div>
  <!--end of container-->
