0af9112558dab0270b04de6219dd3bb5a39a392d
Evad <torres.kim.dave@gmail.com>
2021-07-01 10:30:07 +0800

    Modified products list code.

--- products.php
+++ products.php
@@ -10,7 +10,7 @@ $prod = $store->getAllProducts();
  <meta http-equiv="X-UA-Compatible" content="IE=edge">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <link rel="stylesheet" href="node_modules/bulma/css/bulma.min.css">
- <title>E-Store | List product/title>
+ <title>E-Store | List product/title> </title>
 </head>
 
 <body>
